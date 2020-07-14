7ef7a4e7a4e3bc8a278fdc6aa4ecec1a2efd2812
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2020-07-14 15:25:33 -0400

    prueba 2

--- src/helpers/upload-image/upload-image.service.ts
+++ src/helpers/upload-image/upload-image.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Axios, { AxiosResponse } from 'axios';
 import FormData = require('form-data');
-
+/*
 @Injectable()
 export class UploadImageService {
 
@@ -17,4 +17,27 @@ export class UploadImageService {
         }).catch(err => err);
       }
 
+}
+*/
+@Injectable()
+export class UploadImageService {
+  uploadImage(image: string): Promise<AxiosResponse> {
+    const formData = new FormData();
+    formData.append('image', image);
+    return Axios({
+      method: 'post',
+      url:
+        'https://api.imgbb.com/1/upload?key=96370f6b88cfde1ea6a16a5d0d13bb0f',
+      data: formData,
+      headers: { ...formData.getHeaders() },
+    })
+      .then(response => {
+        console.log(response);
+        return response;
+      })
+      .catch(err => {
+        console.log(err);
+        return err;
+      });
+  }
 }
